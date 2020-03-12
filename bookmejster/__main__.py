42cace3f9780c496290c9aa54fd35443f067e087
ethru <ethru@protonmail.com>
2020-03-12 11:37:13 +0100

    Add main file

---
+++ bookmejster/__main__.py
@@ -0,0 +1,10 @@
+from bookmejster.gui import Gui
+from sys import platform
+
+
+def main():
+    width = '450' if platform == 'win32' else '600'
+    Gui('Bookstore Manager', f'{width}x470').mainloop()
+
+
+if __name__ == '__main__': main()
