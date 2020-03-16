76f785292047a8be3c1c0bb149951e7566ad2338
ethru <ethru@protonmail.com>
2020-03-16 11:06:28 +0100

    Add documentation introduction

--- bookmeister/__init__.py
+++ bookmeister/__init__.py
@@ -1,1 +1,25 @@
+"""Bookstore Manager
+
+#### Introduction
+Application is part of university project. Idea was to create online bookstore. Module presented here is responsible for
+connection with database REST API. It allows to store, load, update and delete records from books collection. Main goal
+of this part was to design simple and intuitive GUI for bookstore employees.
+
+#### Features
+- form validation before sending values to database
+- search by one or many parameters
+- easy edit of data: form autofill when record chosen from search results
+- upload / download (view) of book cover
+- display of notification after performing action
+
+#### License
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 __version__ = '1.0.0'
--- bookmeister/__main__.py
+++ bookmeister/__main__.py
@@ -1,8 +1,26 @@
+"""Main
+
+Contains `main` function which creates application GUI and sets its size. It needs module sys to specify which platform
+is used and adjust application width.
+
+
+#### License
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from bookmeister.gui import Gui
 from sys import platform
 
 
 def main():
+    """Sets GUI width according to used platform and runs it."""
+
     width = '450' if platform == 'win32' else '600'
     Gui('Bookstore Manager', f'{width}x470').mainloop()
 
