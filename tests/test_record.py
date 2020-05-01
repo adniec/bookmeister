88930e4a283b3aa1547bbc4fb8a69403f8a0a526
ethru <ethru@protonmail.com>
2020-05-01 22:04:53 +0200

    Add record integration tests

--- tests/test_record.py
+++ tests/test_record.py
@@ -18,6 +18,19 @@ def is_value_error_raised(request, args):
         return False
 
 
+@pytest.fixture
+def cast_record(args):
+    fields = ('Price', 'ISBN', 'Release', 'Pages', 'Quantity', 'Discount')
+    tested = record.Record()
+    for index, argument in enumerate(args):
+        try:
+            tested[fields[index]] = argument
+        except ValueError:
+            pass
+    tested.cast()
+    return tested
+
+
 def test_fields():
     assert record.FIELDS == ('Title', 'Author', 'Type', 'Publisher', 'ISBN',
                              'Release', 'Language', 'Pages', 'Quantity',
@@ -121,3 +134,123 @@ def test_price_check(raised, expected):
 def test_empty_record():
     tested = record.Record()
     assert tested == {}
+
+
+@pytest.mark.parametrize(('key', 'value', 'expected'), (
+        ('Title', 'Test', {'Title': 'Test'}),
+        ('Title', 'Test' * 50, {}),
+        ('Author', 'Mr Test', {'Author': 'Mr Test'}),
+        ('Type', 'Thriller', {'Type': 'Thriller'}),
+        ('Publisher', 'Test' * 50, {}),
+        ('Release', '2020', {'Release': '2020'}),
+        ('Release', '1800', {'Release': '1800'}),
+        ('Release', '1321', {}),
+        ('Release', '2085', {}),
+        ('Language', 'EN', {'Language': 'EN'}),
+        ('Language', 'ES', {'Language': 'ES'}),
+        ('Language', '12', {}),
+        ('Language', 'E~', {}),
+        ('Pages', '1651', {'Pages': '1651'}),
+        ('Pages', '9999999', {}),
+        ('Pages', '-158', {}),
+        ('Quantity', '10', {'Quantity': '10'}),
+        ('Quantity', 'Test', {}),
+        ('Quantity', '-100', {}),
+        ('Quantity', '45.5', {}),
+        ('Price', '100', {'Price': '100'}),
+        ('Price', '89.99', {'Price': '89.99'}),
+        ('Price', '12.345', {}),
+        ('Discount', '50', {'Discount': '50'}),
+        ('Discount', '-15', {}),
+        ('Discount', '2.5', {}),
+        ('Discount', 'Test', {}),
+        ('Title', '', {}),
+        ('Author', '', {}),
+        ('Type', '', {}),
+        ('Publisher', '', {}),
+        ('ISBN', '', {}),
+        ('Publisher', '', {}),
+        ('Release', '', {}),
+        ('Language', '', {}),
+        ('Pages', '', {}),
+        ('Quantity', '', {}),
+        ('Price', '', {}),
+        ('Discount', '', {}),
+        ('Test', 'Test', {'Test': 'Test'}),
+))
+def test_add_record_value(key, value, expected):
+    tested = record.Record()
+    try:
+        tested[key] = value
+    except ValueError:
+        pass
+    assert tested == expected
+
+
+@pytest.mark.parametrize(('key', 'value'), (
+        ('Title', 'Test' * 50),
+        ('Author', 'Test' * 50),
+        ('Type', 'Test' * 50),
+        ('Publisher', 'Test' * 50),
+        ('Release', 'Test' * 50),
+        ('Language', 'Test'),
+        ('Pages', '1000000'),
+        ('Quantity', '-100'),
+        ('Price', '12.345.6789'),
+        ('Discount', '100'),
+        ('Title', ''),
+        ('Author', ''),
+        ('Type', ''),
+        ('Publisher', ''),
+        ('ISBN', ''),
+        ('Publisher', ''),
+        ('Release', ''),
+        ('Language', ''),
+        ('Pages', ''),
+        ('Quantity', ''),
+        ('Price', ''),
+        ('Discount', ''),
+))
+def test_record_raise_error(key, value):
+    with pytest.raises(ValueError):
+        tested = record.Record()
+        tested[key] = value
+
+
+@pytest.mark.parametrize(('args', 'expected'), (
+        (('12.34', '9780330102360', '2005', '189', '10', '25'), {
+            'Price': 12.34,
+            'ISBN': 9780330102360,
+            'Release': 2005,
+            'Pages': 189,
+            'Quantity': 10,
+            'Discount': 25,
+        }),
+        (('123', '9781856132657', '1800', '-10', 'AB', '99'), {
+            'Price': 123,
+            'ISBN': 9781856132657,
+            'Release': 1800,
+            'Discount': 99,
+        }),
+        (('-10', 'test', '', '1580', '100', '5'), {
+            'Pages': 1580,
+            'Quantity': 100,
+            'Discount': 5,
+        }),
+        (('100', '9780141028279', '1999'), {
+            'Price': 100,
+            'ISBN': 9780141028279,
+            'Release': 1999,
+        }),
+        (('', '', '', '', '', '50'), {
+            'Discount': 50,
+        }),
+        (('150.25', '9780099578031', '876', '762', '5'), {
+            'Price': 150.25,
+            'ISBN': 9780099578031,
+            'Pages': 762,
+            'Quantity': 5,
+        }),
+))
+def test_record_cast_values(cast_record, expected):
+    assert cast_record == expected
